78051052d90ef4da013876fab582007f729decf2
DevinDAJ <daj.softworks@gmail.com>
2020-05-20 11:58:38 +0800

    first commit

---
+++ src/script/view/main.js
@@ -0,0 +1,38 @@
+var main = () => {
+    let searchElement = document.querySelector("#searchElement");
+    let buttonSearchElement = document.querySelector("#searchButtonElement");
+    let clubListElement = document.querySelector("#clubList");
+
+    var onButtonSearchClicked = () => {
+        let dataSource = new DataSource(renderResult, fallbackResult);
+        dataSource.searchClub(searchElement.value);
+    };
+
+    var renderResult = results => {
+        clubListElement.innerHTML = "";
+        results.forEach(club => {
+            let name = "";
+            let fanArt = "";
+            let description = "";
+
+            ({ name, fanArt, description } = club);
+
+            let clubElement = document.createElement("div");
+            clubElement.setAttribute("class", "club");
+
+            clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+                <div class="club-info">
+                <h2>${name}</h2>
+                <p>${description}</p>
+                </div>`;
+            clubListElement.appendChild(clubElement);
+        })
+    };
+
+    var fallbackResult = message => {
+        clubListElement.innerHTML = "";
+        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
+    };
+
+    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+};
